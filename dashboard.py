702d022afd05b5c009a5e8da45abea9f62c23c0b
Ramadhani Ari Putra <127958544+rmdhaniap@users.noreply.github.com>
2025-03-09 20:26:34 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,11 +1,8 @@
-pip install seaborn
-pip install matplotlib
-
 import pandas as pd 
 import numpy as np  
-import matplotlib.pyplot as plt  
-import seaborn as sns  
-sns.set_style("whitegrid")
+#import matplotlib.pyplot as plt  
+#import seaborn as sns  
+#sns.set_style("whitegrid")
 from datetime import datetime
 import scipy.stats as stats
 from sklearn.preprocessing import StandardScaler
